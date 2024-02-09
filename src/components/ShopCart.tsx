19da0ec5b9789dd0c319ffd77043d0d9f6e4393b
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2024-02-09 15:02:11 -0300

    feat: update shopcart

--- src/components/ShopCart.tsx
+++ src/components/ShopCart.tsx
@@ -17,7 +17,7 @@ const ShopCart = () => {
         openNavbar ? 'fixed' : 'hidden'
       } w-full max-w-96 h-screen top-0 right-0 z-30 bg-[#121212]`}
     >
-      <div className="relative py-20 px-10 flex flex-col">
+      <div className="py-20 px-10 flex flex-col">
         <button
           onClick={() => setOpenNavbar(!openNavbar)}
           className="absolute top-6 right-6"
@@ -27,35 +27,33 @@ const ShopCart = () => {
 
         <h1 className="mb-4 text-lg font-semibold">Sacola de compras</h1>
 
-        <div>
-          <ul className="flex flex-col space-y-2">
-            {cartItems.map((item, index) => (
-              <li key={index}>
-                <CartItems carts={item} handleRemoveItem={handleRemoveItem} />
-              </li>
-            ))}
-          </ul>
+        <ul className="flex flex-col max-h-[65vh] overflow-y-auto gap-y-4">
+          {cartItems.map((item, index) => (
+            <li key={index}>
+              <CartItems carts={item} handleRemoveItem={handleRemoveItem} />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="absolute left-0 right-0 bottom-0 py-10 px-10 bg-[#121212] border-t border-t-gray-800">
+        <div className="flex justify-between">
+          <p>Quantidade</p>
+          <p>{qtd} itens</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="text-lg">Valor total</p>
+          <p className="text-lg font-semibold">
+            {valueTotal.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </p>
         </div>
 
-        <div className="absolute -bottom-64 left-6 right-6 p-4 bg-[#121212] z-20">
-          <div className="flex justify-between">
-            <p>Quantidade</p>
-            <p>{qtd} itens</p>
-          </div>
-          <div className="flex justify-between">
-            <p className="text-lg">Valor total</p>
-            <p className="text-lg font-semibold">
-              {valueTotal.toLocaleString('pt-BR', {
-                style: 'currency',
-                currency: 'BRL',
-              })}
-            </p>
-          </div>
-
-          <button className="bg-emerald-600 mt-4 py-2 rounded-md w-full">
-            Finalizar compra
-          </button>
-        </div>
+        <button className="bg-emerald-600 mt-4 py-2 rounded-md w-full">
+          Finalizar compra
+        </button>
       </div>
     </div>
   );
