c57554dea9f47a18beb6e8f4bf485bd5287f142e
Bruno Lima <bruno2.jean.lima@outlook.com>
2024-02-10 17:45:08 -0300

    feat: count of items in cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,18 +6,30 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   const context = useContext(UserContext);
 
-  const { openSidebar, setOpenSidebar } = context!;
+  const { openSidebar, setOpenSidebar, cartItems } = context!;
+
+  const qtd = cartItems.length;
 
   return (
-    <nav className="border-b border-elements-color py-6 px-16">
-      <div className="flex justify-between">
+    <nav className="border-b border-elements-color py-4 px-16">
+      <div className="flex justify-between items-center">
         <Link to="/" className="text-2xl font-semibold text-title-color">
           Store
         </Link>
 
-        <button onClick={() => setOpenSidebar(!openSidebar)} type="button">
-          <Handbag size={30} color="#8D8D99" />
-        </button>
+        <div className="relative">
+          <button
+            onClick={() => setOpenSidebar(!openSidebar)}
+            type="button"
+            className="bg-black/40 p-2 rounded-md"
+          >
+            <Handbag size={30} color="#8D8D99" />
+          </button>
+
+          <span className="text-white text-base bg-primary-color rounded-full py-[1px] px-[8px] absolute -top-2 -right-3">
+            {qtd}
+          </span>
+        </div>
       </div>
     </nav>
   );
